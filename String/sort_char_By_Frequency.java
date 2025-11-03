5834bb61fa99031a808c64aabe4395ae643fa330
milan-sahoo04 <sahoomilan2004@gmail.com>
2025-11-03 11:19:22 +0530

    No modification

--- String/sort_char_By_Frequency.java
+++ String/sort_char_By_Frequency.java
@@ -1,19 +1,20 @@
-import java.util.*;
+// import java.util.*;
 
-class sort_char_By_Frequency {
-    public static String sort_by_freq(String str) {
-        String[] str1 = str.split("");
-        StringBuilder sb = new StringBuilder();
-        for (int i = str1.length - 1; i >= 0; i--) {
-            sb.append(str1[i]);
-        }
-        return sb.toString();
-    }
+// class sort_char_By_Frequency {
+// public static String sort_by_freq(String str) {
+// String[] str1 = str.split("");
+// StringBuilder sb = new StringBuilder();
+// int count = 0;
+// for (int i = 1; i < str1.length; i++) {
+// if()
+// }
+// return sb.toString();
+// }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = "cccaaa";
-        System.out.println(sort_by_freq(str));
-        sc.close();
-    }
-}
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// String str = "cccaaa";
+// System.out.println(sort_by_freq(str));
+// sc.close();
+// }
+// }
